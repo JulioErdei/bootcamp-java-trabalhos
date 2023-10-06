13de16f8130a1f945abe76de90545760eb7bf564
Julio Erdei <juliocezaredei1234@gmail.com>
2023-10-06 18:10:49 -0300

    final organizador ativos

--- Desafio2/Ativos/src/BancoOrganizadorAtivos.java
+++ Desafio2/Ativos/src/BancoOrganizadorAtivos.java
@@ -3,24 +3,24 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class BancoOrganizadorAtivos {
-  /**
-   * @param args
-   */
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     ArrayList < String > ativos = new ArrayList < > ();
     int quantidadeAtivos = scanner.nextInt();
 
-    for (int i = 0; i < quantidadeAtivos; i++) {
+    for (int i = 0; i <= quantidadeAtivos; i++) {
       String codigoAtivo = scanner.nextLine();
       ativos.add(codigoAtivo);
     }
     
-    int i = 0;
-    int j = ativos.size();
-    while (i < j)
-      i++;
-     
+    Collections.sort(ativos);
+    
+
+    for (String ativo : ativos){
+      System.out.println(ativo);
+    }
+    
 
   }
 }
