ebdc3c699ea40af5938c53e361eb46415f051477
Julio Erdei <juliocezaredei1234@gmail.com>
2023-10-21 12:24:24 -0300

    Inicio projeto API

---
+++ Projeto API com Spring3/API-spring-swagger/src/test/java/exe/Spring/APIspringswagger/ApiSpringSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package exe.Spring.APIspringswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
